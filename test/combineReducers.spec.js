d911cb9351fd1418b0025e09ba519b5f5cf5b1cb
Chad Lee <git@chadly.net>
2016-09-05 10:30:11 -0500

    Make combineReducers idempotent

--- test/combineReducers.spec.js
+++ test/combineReducers.spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import Immutable from 'seamless-immutable';
 import combineReducers from '../src/combineReducers';
 
 describe('combineReducers', () => {
@@ -14,5 +15,17 @@ describe('combineReducers', () => {
     expect(s1).to.eql({ counter: 1, stack: [] })
     const s2 = reducer(s1, { type: 'push', value: 'a' })
     expect(s2).to.eql({ counter: 1, stack: [ 'a' ] })
+  })
+
+  it('returns the same object instance if no state has changed', () => {
+    const reducer = combineReducers({
+      doNothing1: state => state || Immutable({ hello: "world" }),
+      doNothing2: state => state || Immutable({ one: 2 })
+    })
+
+    const s1 = reducer(Immutable({}), { type: "test1" });
+    const s2 = reducer(s1, { type: "test2" });
+
+    expect(s1).to.equal(s2);
   })
 })
